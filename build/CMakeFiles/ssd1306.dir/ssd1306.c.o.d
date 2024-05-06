6d5456bd4d4ae0893f8ffbef91d5cd6548fbec3d
Samantha Salas <ssammie47@gmail.com>
2024-05-05 20:56:23 -0500

    made some fixes

---
+++ build/CMakeFiles/ssd1306.dir/ssd1306.c.o.d
@@ -0,0 +1,25 @@
+CMakeFiles/ssd1306.dir/ssd1306.c.o: /app/ssd1306.c \
+ /duo-sdk/rootfs/usr/include/stdc-predef.h \
+ /duo-sdk/riscv64-linux-musl-x86_64/lib/gcc/riscv64-unknown-linux-musl/10.2.0/include/stdint.h \
+ /duo-sdk/rootfs/usr/include/stdint.h \
+ /duo-sdk/rootfs/usr/include/bits/alltypes.h \
+ /duo-sdk/rootfs/usr/include/bits/stdint.h \
+ /duo-sdk/rootfs/usr/include/stdlib.h \
+ /duo-sdk/rootfs/usr/include/features.h \
+ /duo-sdk/rootfs/usr/include/alloca.h \
+ /duo-sdk/rootfs/usr/include/inttypes.h \
+ /duo-sdk/rootfs/usr/include/sys/types.h \
+ /duo-sdk/rootfs/usr/include/endian.h \
+ /duo-sdk/rootfs/usr/include/sys/select.h \
+ /duo-sdk/rootfs/usr/include/sys/stat.h \
+ /duo-sdk/rootfs/usr/include/bits/stat.h \
+ /duo-sdk/rootfs/usr/include/unistd.h \
+ /duo-sdk/rootfs/usr/include/bits/posix.h \
+ /duo-sdk/rootfs/usr/include/stdio.h /duo-sdk/rootfs/usr/include/string.h \
+ /duo-sdk/rootfs/usr/include/strings.h \
+ /duo-sdk/rootfs/usr/include/fcntl.h \
+ /duo-sdk/rootfs/usr/include/bits/fcntl.h \
+ /duo-sdk/rootfs/usr/include/sys/ioctl.h \
+ /duo-sdk/rootfs/usr/include/bits/ioctl.h \
+ /duo-sdk/rootfs/usr/include/bits/ioctl_fix.h /app/linux_i2c.h \
+ /app/ssd1306.h /app/font.h
